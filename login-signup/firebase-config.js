5fa628a82ae5db1053aeb8e651838560c25bcae1
Jennifernwaa <Jennifernwachinaemere@gmail.com>
2025-06-09 12:37:21 +0700

    fixed api error

--- login-signup/firebase-config.js
+++ login-signup/firebase-config.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.8.1/firebase
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.FIREBASE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDn04LCckI_-sxQvHe4frnheCcvQSa6gCc",
+  authDomain: "mybookspace-jennifer.firebaseapp.com",
+  projectId: "mybookspace-jennifer",
+  storageBucket: "mybookspace-jennifer.firebasestorage.app",
+  messagingSenderId: "842011822044",
+  appId: "1:842011822044:web:d801617e044c86be98f119",
+  measurementId: "G-S48BPR0TDX"
 };
 
 // Initialize Firebase
